24f87e1a5011901f018a63ff2724c5f80db7a3ae
JairoBraga <neto.braga08@gmail.com>
2020-11-10 15:27:22 -0300

    Developed and improve methods crud.

---
+++ src/main/java/one/digital/innovation/PersonAPI/mapper/PersonMapper.java
@@ -0,0 +1,20 @@
+package one.digital.innovation.PersonAPI.mapper;
+
+import one.digital.innovation.PersonAPI.DTO.request.PersonDTO;
+import one.digital.innovation.PersonAPI.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import org.springframework.context.annotation.Configuration;
+
+@Mapper(componentModel="spring")
+public interface PersonMapper {
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+
+}
+
